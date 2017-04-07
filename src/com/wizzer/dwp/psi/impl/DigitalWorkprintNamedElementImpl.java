27b8ee8536d6531b100ecd1701bf7b5b64dddee0
Mark Millard <mmillard@alticast.com>
2017-04-07 16:24:35 -0600

    Fixed bug in DWP language named element implementation.

--- src/com/wizzer/dwp/psi/impl/DigitalWorkprintNamedElementImpl.java
+++ src/com/wizzer/dwp/psi/impl/DigitalWorkprintNamedElementImpl.java
@@ -2,56 +2,16 @@ package com.wizzer.dwp.psi.impl;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
-import com.intellij.util.IncorrectOperationException;
-import com.wizzer.dwp.psi.DigitalWorkprintElementFactory;
 import com.wizzer.dwp.psi.DigitalWorkprintNamedElement;
-import com.wizzer.dwp.psi.DigitalWorkprintTypes;
-import com.wizzer.dwp.psi.DwpProperty;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by msm on 3/31/17.
  */
-public class DigitalWorkprintNamedElementImpl extends ASTWrapperPsiElement implements DigitalWorkprintNamedElement
+public abstract class DigitalWorkprintNamedElementImpl extends ASTWrapperPsiElement implements DigitalWorkprintNamedElement
 {
     public DigitalWorkprintNamedElementImpl(@NotNull ASTNode node)
     {
         super(node);
     }
-
-    @Nullable
-    public PsiElement getNameIdentifier() { return null; }
-
-    public PsiElement setName(@NonNls @NotNull String var1) throws IncorrectOperationException
-    { return null; }
-    
-    /*
-    public static String getName(DwpProperty element)
-    {
-        return getKey(element);
-    }
-
-    public static PsiElement setName(DwpProperty element, String newName) {
-        ASTNode keyNode = element.getNode().findChildByType(DigitalWorkprintTypes.KEY);
-        if (keyNode != null) {
-
-            DwpProperty property = DigitalWorkprintElementFactory.createProperty(element.getProject(), newName);
-            ASTNode newKeyNode = property.getFirstChild().getNode();
-            element.getNode().replaceChild(keyNode, newKeyNode);
-        }
-        return element;
-    }
-
-    public static PsiElement getNameIdentifier(DwpProperty element) {
-        ASTNode keyNode = element.getNode().findChildByType(DigitalWorkprintTypes.KEY);
-        if (keyNode != null) {
-            return keyNode.getPsi();
-        } else {
-            return null;
-        }
-    }
-    */
 }
